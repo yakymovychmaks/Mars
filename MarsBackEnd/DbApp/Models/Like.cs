aa9930268a205644dac8c0fc7f6616b789a6fbab
Proces <pocerfeus@gmail.com>
2023-11-04 12:04:59 +0000

    feat: add Like maodel #1.4

---
+++ MarsBackEnd/DbApp/Models/Like.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbApp.Models
+{
+    public class Like
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public Post Post { get; set; }
+    }
+}
