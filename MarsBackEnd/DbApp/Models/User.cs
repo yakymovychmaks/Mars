ed4915c37f6ac99225bc58f8ba11e2513e07b410
Proces <pocerfeus@gmail.com>
2023-11-04 11:34:24 +0000

    #1.1 Added new project Class library and User model

---
+++ MarsBackEnd/DbApp/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DbApp.Models
+{
+    public class User
+    {
+        public int Id {  get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string ProfilePicture {  get; set; }
+        public string UserRole { get; set; }
+        public List<Post> Posts { get; set; }
+        public List<Comment> Comments { get; set; }
+        public List<Like> Likes { get; set; }
+    }
+}
---
+++ MarsBackEnd/MarsBackEnd/DataModels/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarsBackEnd.DataModels
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+        
+    }
+}
