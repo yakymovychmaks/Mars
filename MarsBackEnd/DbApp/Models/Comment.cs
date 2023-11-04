26d7d07c43223110dac9a430d734533cc6a80c53
Proces <pocerfeus@gmail.com>
2023-11-04 12:00:07 +0000

    feat: add comment model #1.3

---
+++ MarsBackEnd/DbApp/Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbApp.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateTime { get; set; }
+        public User User { get; set; }
+        public Post Post { get; set; }
+    }
+}
