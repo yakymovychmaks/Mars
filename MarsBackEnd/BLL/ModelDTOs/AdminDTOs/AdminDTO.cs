28d14e3c76274ed5290a8baa571b71a6395ef5c0
Proces <pocerfeus@gmail.com>
2023-11-12 17:54:29 +0000

    feat: added BLL and UserDTO #6
    I created a new project (Class Library) for BLL and created Admin DTO
    
    In the Bll we have just AdminDto model now but we need made PostsDTO model.

---
+++ MarsBackEnd/BLL/ModelDTOs/AdminDTOs/AdminDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.ModelDTOs.AdminDTOs
+{
+    public class AdminDTO
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<PostsDTO> PostsOnSite { get; set; }
+    }
+}
