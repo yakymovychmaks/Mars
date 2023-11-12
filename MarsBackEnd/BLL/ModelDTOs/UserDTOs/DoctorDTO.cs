72294e3dcb3b06f0557373a3f9677d8651eb4994
Proces <pocerfeus@gmail.com>
2023-11-12 18:06:39 +0000

    feat: Added DoctorDTO model #6
    I created DoctorDTO model. It's need the ApointmentDTO because hase a list of apointment

---
+++ MarsBackEnd/BLL/ModelDTOs/UserDTOs/DoctorDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.ModelDTOs.UserDTOs
+{
+    public class DoctorDTO
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Job { get; set; }
+        public DateTime DAteOfBirth { get; set; }
+        public string ProfilePicture { get; set; }
+        public List<ApointmentDTO> Apointment { get; set; }
+    }
+}
