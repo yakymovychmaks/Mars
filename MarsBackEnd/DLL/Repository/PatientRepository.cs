561f286f6ec179940f59064b6031499c9c9028d6
Proces <pocerfeus@gmail.com>
2023-11-12 15:53:00 +0000

    feat: added Patient Repository #4
    I created Patient Repository for Patient model implements IReposity.

---
+++ MarsBackEnd/DLL/Repository/PatientRepository.cs
@@ -0,0 +1,50 @@
+﻿using DLL.DataAccess;
+using DLL.Interface;
+using DLL.Models.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Repository
+{
+    public class PatientRepository : IRepository<Patient>
+    {
+        private readonly ApplicationDbContext _DbContext;
+        public PatientRepository(ApplicationDbContext dbContext)
+        {
+            _DbContext = dbContext;
+        }
+
+        public void Add(Patient entity)
+        {
+            _DbContext.Patients.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var PatientToDelete = _DbContext.Patients.Find(id);
+            if (PatientToDelete != null)
+            {
+                _DbContext.Patients.Remove(PatientToDelete);
+                _DbContext.SaveChanges();
+            }
+        }
+
+        public IEnumerable<Patient> GetAll()
+        {
+            return _DbContext.Patients.ToList();
+        }
+
+        public Patient GetById(int id)
+        {
+            return _DbContext.Patients.Find(id);
+        }
+
+        public void Update(Patient entity)
+        {
+            _DbContext.Update(entity);
+        }
+    }
+}
