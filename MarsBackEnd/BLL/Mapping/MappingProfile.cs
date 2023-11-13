3ff2e7f1ac022cdf93e34704627f678ab8dfc8ba
Proces <pocerfeus@gmail.com>
2023-11-13 20:00:38 +0000

    feat: added MappingProfile #8
    I added automaper in NuGet Packages. Created the automaper profile for all models and DTOs.
    
    Without automaper i can't write services.

---
+++ MarsBackEnd/BLL/Mapping/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using BLL.ModelDTOs.AdminDTOs;
+using BLL.ModelDTOs.UserDTOs;
+using DLL.Models.AdminsModel;
+using DLL.Models.UserModel;
+
+namespace BLL.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<AdminDTO, Admin>().ReverseMap();
+            CreateMap<PostsDTO, Posts>().ReverseMap();
+            CreateMap<DoctorDTO, Doctor>().ReverseMap();
+            CreateMap<PatientDTO, Patient>().ReverseMap();
+            CreateMap<ApointmentDTO, Apointment>().ReverseMap();
+            CreateMap<OfficeDTO, Office>().ReverseMap();
+        }
+    }
+}
