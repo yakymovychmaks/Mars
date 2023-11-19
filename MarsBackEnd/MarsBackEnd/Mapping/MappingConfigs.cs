d3642282edd687ad7192095428bbd77e1bb57d7c
Proces <pocerfeus@gmail.com>
2023-11-19 12:16:44 +0000

    feat: added APIAutomapper #10
    I created new automapper for all API model models

---
+++ MarsBackEnd/MarsBackEnd/Mapping/MappingConfigs.cs
@@ -0,0 +1,21 @@
+﻿using BLL.ModelDTOs.AdminDTOs;
+using MarsBackEnd.Models.Admin;
+using AutoMapper;
+using MarsBackEnd.Models.Users;
+using BLL.ModelDTOs.UserDTOs;
+
+namespace MarsBackEnd.Mapping
+{
+    public class MappingConfigs : Profile
+    {
+        public MappingConfigs()
+        {
+            CreateMap<AdminAPIModel, AdminDTO>().ReverseMap();
+            CreateMap<PostsAPIModel, PostsDTO>().ReverseMap();
+            CreateMap<DoctorAPIModel,DoctorDTO>().ReverseMap();
+            CreateMap<PatientAPIModel, PatientDTO>().ReverseMap();
+            CreateMap<OfficeAPIModel,OfficeDTO>().ReverseMap();
+            CreateMap<ApointmentAPIModel, ApointmentDTO>().ReverseMap();
+        }
+    }
+}
