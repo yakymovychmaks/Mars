8754bc20d3ae36a170ab8d003cc8ddeae993af53
Proces <pocerfeus@gmail.com>
2023-12-05 21:35:38 +0000

    feat: added all method in comment service #19
    I added all methods for comment, but I'll have to test the update method

--- MarsBackEnd/BLL/Services/CommentService.cs
+++ MarsBackEnd/BLL/Services/CommentService.cs
@@ -1,5 +1,8 @@
-﻿using BLL.Interface;
+﻿using AutoMapper;
+using BLL.Interface;
 using BLL.ModelDTOs.UserDTOs;
+using DLL.Model.UserModel;
+using DLL.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,30 +13,71 @@ namespace BLL.Services
 {
     public class CommentService : IService<CommentDTO>
     {
-        
+        private CommentRepository _commentRepository;
+        private IMapper _mapper;
+        public CommentService (CommentRepository commentRepository, IMapper mapper)
+        {
+            _commentRepository = commentRepository;
+            _mapper = mapper;
+        }
         public string Add(CommentDTO entity)
         {
-            
+            try
+            {
+                return _commentRepository.Add(_mapper.Map<Comment>(entity));
+            }
+            catch (Exception ex)
+            {
+                return "Exeption on the BLL layer" + ex.Message;
+            }
         }
 
         public string Delete(CommentDTO entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _commentRepository.Delete(entity.Id);
+            }
+            catch (Exception ex)
+            {
+                return "Exeption on the BLL layer" + ex.Message;
+            }
         }
 
         public IEnumerable<CommentDTO> GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _mapper.Map<IEnumerable<CommentDTO>>(_commentRepository.GetAll());
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
         }
 
         public CommentDTO GetById(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _mapper.Map<CommentDTO>(_commentRepository.GetById(id));
+            }
+            catch
+            {
+                return null;
+            }
         }
 
         public string Update(CommentDTO entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _commentRepository.Update(_mapper.Map<Comment>(entity));
+            }
+            catch (Exception ex)
+            {
+                return "Exeption on the BLL layer" + ex.Message;
+            }
         }
     }
 }
