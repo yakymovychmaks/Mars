4bb02c6502cfb7b0c814201d2101f8b799bb4343
Proces <pocerfeus@gmail.com>
2023-12-05 22:15:02 +0000

    feat: added Coment API all methods #20
    I created all API methods but without Update

---
+++ MarsBackEnd/MarsBackEnd/APIServices/CommentAPIService.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using BLL.ModelDTOs.UserDTOs;
+using BLL.Services;
+using MarsBackEnd.Models.UserAPIModeles;
+using Newtonsoft.Json;
+
+namespace MarsBackEnd.APIServices
+{
+    public class CommentAPIService
+    {
+        private CommentService _commentService;
+        private IMapper _mapper;
+        public CommentAPIService(CommentService commentService, IMapper mapper)
+        {
+            _commentService = commentService;
+            _mapper = mapper;
+        }
+        public string AddComment(CommentAPImodel commentAPImodel)
+        {
+            return _commentService.Add(_mapper.Map<CommentDTO>(commentAPImodel));
+        }
+        public string DeleteComent(CommentAPImodel commentAPImodel)
+        {
+            return _commentService.Delete(_mapper.Map<CommentDTO>(commentAPImodel));
+        }
+        public string GetAllCommentAsJson()
+        {
+            return JsonConvert.SerializeObject(_mapper.Map<IEnumerable<CommentAPImodel>>(_commentService.GetAll()));
+        }
+        public string GetCommentByIdAsJson(int id)
+        {
+            return JsonConvert.SerializeObject(_mapper.Map<>)
+        }
+    }
+}
