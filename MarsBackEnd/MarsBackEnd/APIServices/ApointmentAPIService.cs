c2b6d7e7a0d69461a51deccd002434d413ec3fb4
Proces <pocerfeus@gmail.com>
2023-12-05 20:44:13 +0000

    feat: added ApointmentAPI #18
     Icreated Apointment API Service and controller, but I didn't create the Update method

---
+++ MarsBackEnd/MarsBackEnd/APIServices/ApointmentAPIService.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using BLL.ModelDTOs.UserDTOs;
+using BLL.Services;
+using MarsBackEnd.Models.UserAPIModeles;
+using Newtonsoft.Json;
+
+namespace MarsBackEnd.APIServices
+{
+    public class ApointmentAPIService
+    {
+        private ApointmentService _apointmentService;
+        private IMapper _mapper;
+
+        public ApointmentAPIService(ApointmentService apointmentService, IMapper mapper)
+        {
+            _apointmentService = apointmentService;
+            _mapper = mapper;
+        }
+        public string AddApointment(ApointmentAPIModel apointmentAPIModel)
+        {
+            if (apointmentAPIModel == null)
+            {
+                return "Sorry";
+            }
+            return _apointmentService.Add(_mapper.Map<ApointmentDTO>(apointmentAPIModel));
+        }
+        public string DeleteApointment(ApointmentAPIModel apointmentAPIModel)
+        {
+            return _apointmentService.Delete(_mapper.Map<ApointmentDTO>(apointmentAPIModel));
+        }
+        public string GetAllApointmentAsJosn()
+        {
+            return JsonConvert.SerializeObject(_apointmentService.GetAll());
+        }
+        public string GetApointmentBuIdAsJson(int id)
+        {
+            return JsonConvert.SerializeObject(_apointmentService.GetById(id));
+        }   
+        public string Update(ApointmentAPIModel apointmentAPIModel)
+        {
+            return _apointmentService.Update(_mapper.Map<ApointmentDTO>(apointmentAPIModel));
+        }
+    }
+}
