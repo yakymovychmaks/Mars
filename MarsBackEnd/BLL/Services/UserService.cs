c0c81bff061ca30fd058cb26d113c66238f92717
Proces <pocerfeus@gmail.com>
2023-12-23 18:04:21 +0000

    feat: userService #33
    I created user service for new async reposoitory and returned base responce

--- MarsBackEnd/BLL/Services/UserService.cs
+++ MarsBackEnd/BLL/Services/UserService.cs
@@ -2,6 +2,7 @@
 using DLL.Interface;
 using Domain.Entity;
 using Domain.Enum;
+using Domain.Extensions;
 using Domain.Helpers;
 using Domain.Response;
 using Microsoft.EntityFrameworkCore;
@@ -11,12 +12,12 @@ namespace BLL.Services
 {
     public class UserService : IUserService
     {
-        private readonly ILogger<UserService> _loger;
+        private readonly ILogger<UserService> _logger;
         private readonly IRepository<Profiles> _profilesRepository;
         private readonly IRepository<User> _userRepository;
-        public UserService(ILogger<UserService> loger, IRepository<Profiles> profilesRepository, IRepository<User> userRepository)
+        public UserService(ILogger<UserService> logger, IRepository<Profiles> profilesRepository, IRepository<User> userRepository)
         {
-            _loger = loger;
+            _logger = logger;
             _profilesRepository = profilesRepository;
             _userRepository = userRepository;
         }
@@ -43,22 +44,107 @@ namespace BLL.Services
                     Posts = new List<Post>()
                 };
                 await _userRepository.Create(user);
+                await _profilesRepository.Create(model.Profile);
+                return new BaseResponse<User>()
+                {
+                    Data = user,
+                    Description = "Користувача створено",
+                    StatusCode = StatusCode.OK
+                };
             }    
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"[UserService.Create] error: {ex.Message}");
+                return new BaseResponse<User>()
+                {
+                    StatusCode = StatusCode.InternalServerError,
+                    Description = $"Внутрішня помилка: {ex.Message}"
+                };
+            }
         }
 
-        public Task<IBaseResponse<bool>> DeleteUser(long id)
+        public async Task<IBaseResponse<bool>> DeleteUser(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var user = await _userRepository.GetById(id);
+                if (user == null)
+                    return new BaseResponse<bool>
+                    {
+                        StatusCode = StatusCode.UserNotFound,
+                        Data = false
+                    };
+                await _userRepository.Delete(user);
+
+                _logger.LogInformation($"[UserService.DeleteUser] користувача видалено");
+
+                return new BaseResponse<bool>
+                {
+                    StatusCode = StatusCode.OK,
+                    Data = true
+                };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"[UserSerivce.DeleteUser] error: {ex.Message}");
+                return new BaseResponse<bool>()
+                {
+                    StatusCode = StatusCode.InternalServerError,
+                    Description = $"Внутренняя ошибка: {ex.Message}"
+                };
+            }
         }
 
         public BaseResponse<Dictionary<int, string>> GetRoles()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var role = ((Role[])Enum.GetValues(typeof(Role)))
+                    .ToDictionary(k => (int)k, t => t.GetDisplayName());
+                return new BaseResponse<Dictionary<int, string>>()
+                {
+                    Data = role,
+                    StatusCode = StatusCode.OK,
+                };
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<Dictionary<int, string>>()
+                {
+                    Description = ex.Message,
+                    StatusCode = StatusCode.InternalServerError
+                };
+            }
         }
 
-        public Task<BaseResponse<IEnumerable<User>>> GetUsers()
+        public async Task<BaseResponse<IEnumerable<User>>> GetUsers()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var users = await _userRepository.GetAll()
+                    .Select(x => new User()
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        Role = x.Role
+                    })
+                    .ToListAsync();
+                _logger.LogInformation($"[UserService.GetUsers] отримано елементів {users.Count}");
+                return new BaseResponse<IEnumerable<User>>()
+                {
+                    Data = users,
+                    StatusCode = StatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"[UserSerivce.GetUsers] error: {ex.Message}");
+                return new BaseResponse<IEnumerable<User>>()
+                {
+                    StatusCode = StatusCode.InternalServerError,
+                    Description = $"Внутрішня помилка: {ex.Message}"
+                };
+            }
         }
     }
 }
