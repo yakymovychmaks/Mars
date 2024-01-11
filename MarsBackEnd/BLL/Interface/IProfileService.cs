b5e24e09e62fd81f2948c02e41e7095c39a8fbc0
Andriy <malyavsky4@gmail.com>
2024-01-11 16:07:09 +0100

    Created new interfaces, services
    Зроблені інтерфейси до всіх моделей, всі сервайси імплементовані(ВСІ КРІМ Apointment). Проте у всіх методів крім ProfileService.cs немає готового коду.

---
+++ MarsBackEnd/BLL/Interface/IProfileService.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entity;
+using Domain.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interface
+{
+    internal interface IProfileService
+    {
+        Task<BaseResponse<Profile>> GetProfile(int id);
+
+        Task<BaseResponse<Profile>> Save(Profile profile);
+    }
+}
