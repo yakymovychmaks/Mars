c4f00ba34a9cb46c09a5fd4e59c269416aecc5f3
Proces <pocerfeus@gmail.com>
2024-01-28 21:27:14 -0600

    feat: CommentService #35
    I created new CommentService with all methods

--- MarsBackEnd/BLL/Interface/ICommentService.cs
+++ MarsBackEnd/BLL/Interface/ICommentService.cs
@@ -3,6 +3,7 @@ using Domain.Response;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,10 +11,10 @@ namespace BLL.Interface
 {
     internal interface ICommentService
     {
-        Task<IBaseResponse<Comment>> Create(Comment comment);
+        Task<IBaseResponse<Comment>> Create(Comment comment, ClaimsPrincipal claimsPrincipal);
 
-        Task<IBaseResponse<Comment>> Update(Comment comment);
-        Task<IBaseResponse<bool>> Delete(int id);
+        //Task<IBaseResponse<Comment>> Update(Comment comment, ClaimsPrincipal claimsPrincipal);
+        Task<IBaseResponse<bool>> Delete(int id, ClaimsPrincipal claimsPrincipal);
 
         Task<IBaseResponse<Comment>> GetComment(int id);
 
--- MarsBackEnd/BLL/Services/CommentService.cs
+++ MarsBackEnd/BLL/Services/CommentService.cs
@@ -5,10 +5,12 @@ using Domain.Entity;
 using Domain.Enum;
 using Domain.Helpers;
 using Domain.Response;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,35 +20,181 @@ namespace BLL.Services
     {
 
         private readonly IRepository<Comment> _commentRepository;
+        private readonly IRepository<Post> _postRepository;
         private readonly ILogger<CommentService> _commentLogger;
-        public CommentService(IRepository<Comment> commentRepository, ILogger<CommentService> commentLogger)
+        public CommentService(IRepository<Comment> commentRepository, ILogger<CommentService> commentLogger, IRepository<Post> postRepository)
         {
+            _postRepository = postRepository;
             _commentRepository = commentRepository;
             _commentLogger = commentLogger;
         }
-        public Task<IBaseResponse<Comment>> Create(Comment comment)
+        public async Task<IBaseResponse<Comment>> Create(Comment comment, ClaimsPrincipal claimsPrincipal)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (claimsPrincipal.IsInRole("admin") || claimsPrincipal.IsInRole("User"))
+                {
+                    var post = _postRepository.GetById(comment.Id);
+                    if(post != null)
+                    {
+                        await _commentRepository.Create(comment);
+                        return new BaseResponse<Comment>()
+                        {
+                            Data = comment,
+                            Description = "Коментар успішно створено",
+                            StatusCode = StatusCode.OK
+                        };
+                    }
+                    else
+                    {
+                        return new BaseResponse<Comment>()
+                        {
+                            Data = null,
+                            Description = "Такого поста не існує",
+                            StatusCode = StatusCode.OK
+                        };
+                    }
+                }
+                else
+                {
+                    return new BaseResponse<Comment>()
+                    {
+                        Data = null,
+                        Description = "У вас недостатньо прав для створення коментарая",
+                        StatusCode = StatusCode.OK
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                _commentLogger.LogError($"[CommentService.Create] error: {ex.InnerException}");
+                return new BaseResponse<Comment>()
+                {
+                    Description = ex.Message,
+                    StatusCode = StatusCode.InternalServerError
+                };
+            }
         }
 
-        public Task<IBaseResponse<bool>> Delete(int id)
+        public async Task<IBaseResponse<bool>> Delete(int id, ClaimsPrincipal claimsPrincipal)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if(claimsPrincipal.IsInRole("admin") || claimsPrincipal.IsInRole("Moderator"))
+                {
+                    var result = await _commentRepository.GetById(id);
+                    if(result != null)
+                    {
+                        await _commentRepository.Delete(result);
+                        return new BaseResponse<bool>()
+                        {
+                            Data = true,
+                            Description = "Комент успішно видалено",
+                            StatusCode = StatusCode.OK
+                        };
+                    }
+                    else
+                    {
+                        return new BaseResponse<bool>()
+                        {
+                            Data = false,
+                            Description = "Такого коментаря не існує",
+                            StatusCode = StatusCode.OK
+                        };
+                    }
+                }
+                else
+                {
+                    return new BaseResponse<bool>()
+                    {
+                        Data = false,
+                        Description = "Ви не можете видалити цей коментар",
+                        StatusCode = StatusCode.OK
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                _commentLogger.LogError(ex,$"[Comment.Delete] error: {ex.Message}");
+                return new BaseResponse<bool>()
+                {
+                    Data = false,
+                    Description = ex.Message,
+                    StatusCode = StatusCode.InternalServerError
+                };
+            }
         }
 
-        public Task<IBaseResponse<IEnumerable<Comment>>> GetAll()
+        public async Task<IBaseResponse<IEnumerable<Comment>>> GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var comment = await _commentRepository.GetAll()
+                    .Select(x => new Comment
+                    {
+                        Id = x.Id,
+                        Description = x.Description,
+                        PostId = x.PostId
+                    })
+                    .ToListAsync();
+                return new BaseResponse<IEnumerable<Comment>>()
+                {
+                    Data = comment,
+                    Description = "Ось всі коментарі",
+                    StatusCode = StatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                _commentLogger.LogError(ex, $"[CommentService.GetAll] error: {ex.Message}");
+                return new BaseResponse<IEnumerable<Comment>>()
+                {
+                    Data = null,
+                    Description = ex.Message,
+                    StatusCode = StatusCode.InternalServerError
+                };
+            }
         }
 
-        public Task<IBaseResponse<Comment>> GetComment(int id)
+        public async Task<IBaseResponse<Comment>> GetComment(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var result = await _commentRepository.GetById(id);
+                if (result == null)
+                {
+                    return new BaseResponse<Comment>()
+                    {
+                        Data = null,
+                        Description = "Такого коментаря не існує",
+                        StatusCode = StatusCode.OK
+                    };
+                }
+                return new BaseResponse<Comment>()
+                {
+                    Data = result,
+                    Description = "Ось коментар",
+                    StatusCode = StatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                _commentLogger.LogError(ex, $"[CommentService.GetComment] error: {ex.Message}");
+                return new BaseResponse<Comment>()
+                {
+                    Data = null,
+                    Description = ex.Message,
+                    StatusCode = StatusCode.InternalServerError
+                };
+            }
         }
 
-        public Task<IBaseResponse<Comment>> Update(Comment comment)
-        {
-            throw new NotImplementedException();
-        }
+        //public Task<IBaseResponse<Comment>> Update(Comment comment, ClaimsPrincipal claimsPrincipal)
+        //{
+        //    try
+        //    {
+
+        //    }
+        //}
     }
 }
