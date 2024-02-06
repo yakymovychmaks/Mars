49c1d779dc786615378793d9f5121a1a4520664d
Proces <pocerfeus@gmail.com>
2024-02-05 22:31:55 -0600

    feat: PhotoService.SevePhotoAsync #23

--- MarsBackEnd/Domain/Entity/Photo.cs
+++ MarsBackEnd/Domain/Entity/Photo.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string FileName { get; set; }
         public string FilePath { get; set; }
-        public DateOnly UploadDate { get; set; }
+        public DateTime UploadDate { get; set; }
         public string Title { get; set; }
 
     }
---
+++ MarsBackEnd/MarsBackEnd/Utilities/PhotoHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace MarsBackEnd.Utilities
+{
+    public class PhotoHelper
+    {
+        private readonly IWebHostEnvironment _environment;
+        private readonly string _uploadPath;
+        public PhotoHelper(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+            _uploadPath = Path.Combine(_environment.WebRootPath, "uploads");
+        }
+        public async Task<string> SendSettingsForSavePhoto()
+        {
+            try
+            {
+                var result = await _photoService.SevePhoto(_uploadPath);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
