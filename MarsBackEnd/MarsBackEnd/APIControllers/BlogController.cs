afdf8ecc8f19076b534ba941df85e2ab13675e62
Proces <pocerfeus@gmail.com>
2024-02-06 21:56:35 -0600

    feat: blog controller #35

---
+++ MarsBackEnd/MarsBackEnd/APIControllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarsBackEnd.APIControllers
+{
+    [Route("api/blog")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+
+    }
+}
